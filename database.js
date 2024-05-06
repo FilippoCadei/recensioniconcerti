0bcd4bcb2e34082e06597e8c67fa9383fd784888
msal28 <marcosalvetti05@gmail.com>
2024-05-06 10:46:54 +0200

    add

--- database.js
+++ database.js
@@ -1,17 +1,17 @@
-    const mysql = require('mysql2');
-    const { HOST, USERNAME, PASSWORD, DBNAME, PORT } = process.env;
-    console.log();
-    const connpool = mysql.createPool({
-        host: "10.20.2.175",
-        user: "tps",
-        password: "ttppss",
-        database: "recensioniconcerti"
-    }, { debug: true });
-    
-    connpool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-        if (error) throw error;
-        console.log('Db is connected - The solution is: ', results[0].solution);
-    });
-    
-    
-    module.exports = connpool;
+const mysql = require('mysql2');
+const { HOST, USERNAME, PASSWORD, DBNAME, PORT } = process.env;
+console.log();
+const connpool = mysql.createPool({
+    host: "10.20.2.174",
+    user: "tps",
+    password: "ttppss",
+    database: "recensioniconcerti"
+}, { debug: true });
+
+connpool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+    if (error) throw error;
+    console.log('Db is connected - The solution is: ', results[0].solution);
+});
+
+
+module.exports = connpool;
